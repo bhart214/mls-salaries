6d6f835df5b5a29cf6d73fcced57898cee9863ff
Brian Hart <bhart214@gmail.com>
2016-05-27 12:32:00 -0400

    more cleaning (full rosters only) further exploration

--- clean.R
+++ clean.R
@@ -8,16 +8,15 @@ mls = read_rds("mls.rds")
 str(mls)
 summary(mls)
 
-# Change Year to factor variable
-mls$Year = as.factor(mls$Year)
-
 # Create new variable for difference between guaranteed_compensation and base_salary
 mls = mls %>% mutate(diff = guaranteed_compensation - base_salary)
 
 summary(mls)
 
-# Missing Data
+# Missing Data - 35 records with missing values, but only one missing salary.
 mls[complete.cases(mls) == FALSE,]
+mls = mls[is.na(mls$base_salary) == FALSE, ]
+
 
 # Club Names
 unique(mls$club)
@@ -44,18 +43,33 @@ mls[mls == "Pool"] = "POOL"
 # Filter out NA, None, and POOL from club column
 mls = mls %>% filter(!grepl("NA|None|POOL", club))
 
-# Positions
-unique(mls$position)
-mls$position[mls$position == "MF"] = "M-F"
-mls$position[mls$position == "M-D"] = "D-M"
-mls$position[mls$position == "F-M"] = "M-F"
-mls$position[mls$position == "M/F"] = "M-F"
-mls$position[mls$position == "M/D"] = "D-M"
-mls$position[mls$position == "D/M"] = "D-M"
-mls$position[mls$position == "D/F"] = "D-F"
-mls$position[mls$position == "F/M"] = "M-F"
-mls$position[mls$position == "F-D"] = "D-F"
 
+
+# Positions
+# This is definitely an oversimplification, but I will convert all positions to GK, D, M, or F and
+# will not allow any combinations.  The first position listed is the default.
+unique(mls$position)
+mls$position[mls$position == "MF" | mls$position == "M-D" | mls$position == "M-F" | mls$position == "M/F" | mls$position == "M/D"] = "M"
+mls$position[mls$position == "D/M" | mls$position == "D-F" | mls$position == "D/F" | mls$position == "D-M"] = "D"
+mls$position[mls$position == "F/M" | mls$position == "F-D" | mls$position == "F-M"] = "F"
+mls = mls[is.na(mls$position) == FALSE, ]
+
+
+
+# Remove records where there were not full rosters.
+# For example, ATL only has 3 players on the payroll.  This could skew the stats.
+table(mls$club, mls$Year)
+
+df_list = list()
+for (i in 2007:2016) {
+  mls_sub = subset(mls, Year == i)
+  tab = table(mls_sub$club)
+  mls_sub = mls_sub[mls_sub$club %in% names(tab)[tab > 20],]
+  df_list[[i]] = mls_sub
+}
+mls = bind_rows(df_list)
+
+table(mls$club, mls$Year)
 
 write_rds(mls, "mls_clean.rds")
 
--- explore.R
+++ explore.R
@@ -1,5 +1,6 @@
 # Data Exploration
 
+library(dplyr)
 library(readr)
 library(ggplot2)
 library(plotly)
@@ -8,7 +9,14 @@ library(ggplotly)
 
 mls = read_rds("mls_clean.rds")
 
+# Total base salaries by club by year
+mls %>% filter(Year %in% c(2008:2016)) %>% group_by(club, Year) %>% summarize(total = sum(base_salary)) %>% 
+  ggplot(aes(x = club, y = total, fill = club)) + geom_bar(stat = "identity") + facet_wrap(~Year)
 
+# Total base salaries by club in 2010
+mls %>% filter(Year == 2010) %>% group_by(Year, club) %>% summarize(total = sum(guaranteed_compensation)) %>% arrange(total)
+
+# Median base salary by club each year
 mls %>% select(club, Year, position, base_salary) %>% 
   group_by(club, Year) %>% 
   summarise(med_base = median(base_salary)) %>% 
@@ -16,34 +24,53 @@ mls %>% select(club, Year, position, base_salary) %>%
   ggvis(~Year, ~med_base) %>% 
   layer_lines(stroke = ~club) 
 
+# Histogram of log(base_salary) by year
+ggplot(data = mls, aes(x = log(base_salary))) + geom_histogram() + facet_wrap(~Year)
 
-table(mls$club, mls$Year)
-
-ggplot(data = mls, aes(x = (base_salary))) + geom_histogram() + facet_wrap(~Year)
-
+# Histogram of log(base_salary) by club in 2016
 mls %>% filter(Year == 2016) %>% 
   ggplot(aes(x = log(base_salary))) + geom_histogram() + facet_wrap(~club)
 
+
+
+# Salary By Position
+mls$position <- factor(mls$position, levels = c("GK","D", "M", "F"), ordered = TRUE)
+
 ggplot(data = mls, aes(x = reorder(club, base_salary, FUN = median), y = (base_salary), color=position)) + geom_boxplot()
 
+ggplot(data = mls, aes(x = position, y = log(base_salary), color = position)) + 
+  geom_boxplot() + geom_point(position = "jitter", alpha = 0.15) + 
+  facet_grid(.~Year) +
+  labs(x = "Position", y = "log(Base Salary)") + ggtitle("MLS Salaries by Position (2007-2016)") +
+  theme(panel.background = element_rect(fill = "grey97"), legend.title=element_blank())
 
 
+# ggplotly interactive of median salary for each club by year
 plot = mls %>% select(Year, club, base_salary) %>% group_by(Year, club) %>% summarize(med_sal = median(base_salary)) %>% 
   ggplot(aes(x = Year, y = med_sal, group = club)) + geom_line(aes(color = club))
 ggplotly(plot)
 
-mls %>% filter(club == "TFC", Year == 2012)
 
-
+# Boxplot of salary for each club (all years)
 ggplot(data = mls, aes(x = club, y = log(base_salary))) + geom_boxplot()
 
+# Heatmaps of salary by position by club
+ggplot(data = mls, aes(club, position)) + 
+  geom_tile(aes(fill = log(base_salary)), color = "white") +
+  scale_fill_gradient(low = "white", high = "steelblue")
 
 ggplot(data = mls, aes(club, position)) + 
   geom_tile(aes(fill = log(base_salary)), color = "white") +
   scale_fill_gradient(low = "white", high = "steelblue") +
   facet_wrap(~ Year)
 
+# Top 50 guaranteed compensations 2007-2016
+mls %>% top_n(n = 50, guaranteed_compensation) %>% arrange(desc(guaranteed_compensation)) %>% print(n=50)
 
+
+# stats: median, mean, range, sd
+# numeric vars: base salary, guaranteed salary
+# categorical variables: club, position, year
 
 # Questions
 # Which teams pay the most? least?
@@ -51,3 +78,4 @@ ggplot(data = mls, aes(club, position)) +
 # which teams have the most variable salaries? least variable?
 # how have salaries changed over time?
 # what if we only look at players making less than $1 million?
+# which players had the greatest changes in salary?
